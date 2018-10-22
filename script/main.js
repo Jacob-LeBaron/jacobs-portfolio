9ba4b19633a1a24d7160705362fe02c8da9b7eba
Jacob LeBaron <34151183+Jacob-LeBaron@users.noreply.github.com>
2018-10-22 16:14:40 -0600

    10/22/2018
    Creation of Javascript File

---
+++ script/main.js
@@ -0,0 +1,13 @@
+Import { films } from `./assets/fims.js`
+Import { starships } from `./assets/fims.js`
+Import { people } from `./assets/fims.js`
+
+Let filmList = document.querySelector(“#film-list”)
+
+films.forEach((film) => {
+     Let listItem = document.createElement(`li`);
+	listItem.textContent = film.opening_crawl
+	filmList.appendChild(listItem)
+console.log(film.title)})
+
+Console.log(people[9]);
