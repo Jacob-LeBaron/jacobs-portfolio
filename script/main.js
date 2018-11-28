2944b1f7bb6efaf7ef67d9a553ffbff93abced63
Jacob LeBaron <jakegobogoth@hotmail.com>
2018-11-27 19:49:43 -0700

    11/27/2018, additions

--- script/main.js
+++ script/main.js
@@ -1,6 +1,10 @@
-import "../Pokemon-DB-master/pokedex";
+import { pokedex } from '../Pokemon-DB-master/pokedex.js'
 
 var card = document.querySelector('.card');
 card.addEventListener( 'mouseover', function() {
   card.classList.toggle('is-flipped');
+});
+
+card.addEventListener('click', function() {
+  card.classList.toggle('flipped')
 });
